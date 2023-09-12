a1322df1874b154e0cd5fc6847fa0bd3da654de8
lightScout <juanmullercs@hotmail.com>
2023-09-12 15:22:50 +0100

    add SpaceXSDK

---
+++ shared/src/commonMain/kotlin/com/lightscout/kmm/shared/SpaceXSDK.kt
@@ -0,0 +1,24 @@
+package com.lightscout.kmm.shared
+
+import com.lightscout.kmm.shared.cache.Database
+import com.lightscout.kmm.shared.cache.DatabaseDriverFactory
+import com.lightscout.kmm.shared.entity.RocketLaunch
+import com.lightscout.kmm.shared.network.SpaceXApi
+
+class SpaceXSDK (databaseDriverFactory: DatabaseDriverFactory) {
+    private val database = Database(databaseDriverFactory)
+    private val api = SpaceXApi()
+
+    @Throws(Exception::class)
+    suspend fun getLaunches(forceReload: Boolean): List<RocketLaunch> {
+        val cachedLaunches = database.getAllLaunches()
+        return if (cachedLaunches.isNotEmpty() && !forceReload) {
+            cachedLaunches
+        } else {
+            api.getAllLaunches().also {
+                database.clearDatabase()
+                database.createLaunches(it)
+            }
+        }
+    }
+}
